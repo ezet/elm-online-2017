3ca1f54c997ba06c9f16c6ae4bf43cb9faf6613e
Lars-Kristian Dahl <lars.dahl@gmail.com>
2017-09-21 19:28:04 +0200

    Task2

--- src/Main.elm
+++ src/Main.elm
@@ -1,1 +1,59 @@
 module Main exposing (..)
+
+import Html exposing (Html, main_, header, h1, text, div, button)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+
+
+type alias Model = { counter1: Int, counter2: Int }
+
+
+init : Model
+init = { counter1 = 0, counter2 = 0}
+
+-- update
+
+
+type Msg
+    = Increment1
+    | Decrement1
+    | Increment2
+    | Decrement2
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment1 ->
+            { model | counter1 = model.counter1 + 1 }
+        Decrement1 ->
+            { model | counter1 = model.counter1 - 1 }
+        Increment2 ->
+            { model | counter2 = model.counter2 + 1 }
+        Decrement2 ->
+            { model | counter2 = model.counter2 - 1 }
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    main_ []
+        [ h1 [] [ text "Counter" ]
+        , div [ class "counter" ]
+            [ button [ onClick Increment1 ] [ text "+" ]
+            , text (toString model.counter1)
+            , button [ onClick Decrement1 ] [ text "-" ]
+            ]
+        , div [class "counter"]
+            [ button [ onClick Increment2 ] [ text "+" ]
+            , text (toString model.counter2)
+            , button [ onClick Decrement2 ] [ text "-" ]
+            ]            
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = init, view = view, update = update }
